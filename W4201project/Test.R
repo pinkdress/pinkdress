de0cc4bf1f8367ad1b6249ff8403b990ccfcc922
HideHN2262 <hn2262@columbia.edu>
2014-09-17 16:48:46 -0400

    Merge branch 'master' of https://github.com/W4201Project2014/W4201-Project
    This reverts commit 4fa919ffe08a8438f28966bb769822a66f612944.
    
    Conflicts:
    W4201project/Test.R

--- W4201project/Test.R
+++ W4201project/Test.R
@@ -1,1 +1,18 @@
 x <-1
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+y <-2
+>>>>>>> parent of 4fa919f... Test by Xiaotong 2
+
+Z <-3
+=======
+## hahahahaah
+>>>>>>> FETCH_HEAD
+=======
+## hahahahaah
+#yes
+
+y<-3
+>>>>>>> FETCH_HEAD
